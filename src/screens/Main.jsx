8c7d3a238ca9e0c8a45efaf831bae99b468a102a
anna_son <anna.son@jeonbotdae.com>
2021-12-01 13:56:04 +0900

    main

--- src/screens/Main.jsx
+++ src/screens/Main.jsx
@@ -1,108 +1,126 @@
-import React, { useState, useEffect } from "react";
-import "../styles/css/main.scss";
+import React, { useState, useEffect, useRef } from "react";
+import { useHistory } from "react-router-dom";
+import "../styles/css/index.scss";
 
-import { ntfList, ntfData } from "../services/collection";
-import DetailModal from "../screens/components/DetailModal";
-import QrModal from "../screens/components/QrModal";
+import { nftAuctionList } from "../services/collection";
 
-import transData from "../config/tranLang";
-import { languageState } from "../store/app";
-import { useRecoilValue } from "recoil";
+const dday = new Date("Decemver 10, 2021, 13:00:00"); //디데이
+// UTC 타입 현재 날짜 (고정)
+const curr = new Date();
+const today = new Date(curr.getTime() + curr.getTimezoneOffset() * 60 * 1000);
 
-function Main() {
+console.log(dday);
+console.log(today);
+
+function Main(props) {
+  const history = useHistory();
+
   // 카운트 제어
-  const dday = new Date("Sep 11,2021,21:00:00"); //디데이
-  const now = new Date(); //현재 날짜 가져오기
-  const distance = dday.getTime() - now;
   const [d, setD] = useState(0);
   const [h, setH] = useState(0);
   const [m, setM] = useState(0);
   const [s, setS] = useState(0);
-
-  // 번역 제어
-  const language = useRecoilValue(languageState);
+  const countTimerRef = useRef();
 
   // 데이터 제어
   const [collectionList, setCollectionList] = useState(null);
-  const closed = dday.getMonth() !== now.getMonth() || dday.getDate() < now.getDate() ? true : false;
-  // const [soldOut, setSoldOut] =  useState(false);
+  const [collectionCtrl, setCollectionCtrl] = useState({
+    0: true,
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+  });
 
-  // 팝업1 제어
-  const [popup1Open, setPopup1Open] = useState(false);
-  const [popup1Data, setPopup1Data] = useState(null);
-
-  // 팝업2 제어
-  const [popup2Open, setPopup2Open] = useState(false);
-  const [popup2Data, setPopup2Data] = useState(null);
-
-  // 코인 리스트 조회
   useEffect(() => {
-    (async () => {
-      // counter();
-
-      const { data } = await ntfList();
-      if (data && data.return_code === 200) setCollectionList(data.response);
-    })();
+    counter();
   }, []);
 
   // 메인 카운트다운
-  // eslint-disable-next-line no-unused-vars
   const counter = () => {
-    setInterval(function () {
+    countTimerRef.current = setInterval(function () {
+      const curr = new Date();
+      const now = new Date(curr.getTime() + curr.getTimezoneOffset() * 60 * 1000);
+
+      const distance = dday.getTime() - now;
+
       setD(Math.floor(distance / (1000 * 60 * 60 * 24)));
       setH(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
       setM(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
-      setS(Math.floor((distance % (1000 * 60)) / 1000));
-      if (s < 10) {
-        setS("0" + s);
-      }
+      setS(
+        Math.floor((distance % (1000 * 60)) / 1000) < 10
+          ? `0${Math.floor((distance % (1000 * 60)) / 1000)}`
+          : Math.floor((distance % (1000 * 60)) / 1000)
+      );
     }, 1000);
   };
 
-  // 코인 세부 정보 조회
-  const collectClick = async (nftId) => {
-    const { data } = await ntfData(nftId);
+  // 컬렉션 클릭시 데이터 로컬스토리지 저장
+  const detailViewDataSet = (data) => {
+    clearInterval(countTimerRef.current);
 
-    if (data && data.return_code === 200) {
-      const detilData = { ...data.response, closed: closed };
-      setPopup1Data(detilData);
-      setPopup1Open(true);
+    const storageData = {
+      nftId: data.nftId,
+      auctionId: data.auctionId,
+      auctionDetailId: data.auctionDetailId,
+      collectionId: data.collectionId,
+      sdate: data.startDate,
+      edate: data.endDate,
+    };
+
+    window.localStorage.setItem("auctionData", JSON.stringify(storageData));
+
+    history.push({
+      pathname: `/nft/${data.nftId}`,
+    });
+  };
+
+  // 컬렉션 날짜 체크
+  const detailOpenChk = (dateVal, type, val) => {
+    const startDate = new Date(dateVal.startDate.replace(/-/g, "/"));
+    const endDate = new Date(dateVal.endDate.replace(/-/g, "/"));
+
+    if (type === "box") {
+      if (startDate < today) {
+        if (endDate < today) {
+          return <div className="redBox">Close</div>;
+        } else return <div className="blueBox">Proceeding</div>;
+      } else {
+        return <div className="greyBox">Getting Ready</div>;
+      }
+    }
+
+    if (type === "ada") {
+      const amount = val.highstBidPrice === 0 ? "BID" : `${val.highstBidPrice / 1000000}ADA`;
+
+      if (startDate < today) {
+        if (endDate < today) {
+          return (
+            <div className="adaBox" style={{ color: "#ff4747" }}>
+              {amount}
+            </div>
+          );
+        } else
+          return (
+            <div className="adaBox" style={{ color: "#0056ff" }}>
+              {amount}
+            </div>
+          );
+      }
     }
   };
 
-  // QR 모달 오픈
-  const openQrModal = (data) => {
-    setPopup1Open(false);
-    setPopup2Open(true);
-    setPopup2Data(data);
-  };
-
   return (
-    //  {/* <p className="countDown">{`${d}Days ${h}:${m}:${s}`}</p> */}
-
-    <div className="main">
-      {/* <DetailModal open={true} /> */}
-
-      <div className="contentsWrap">
-        <div className="mainContents">
-          <div className="iconScroll" />
-          <div className="mainImg" />
-          <div className="mainInner">
-            <div className="mainInnerTop">
-              <div className="title">
-                Cardano nft
-                <br />
-                for charity
-              </div>
-              <div className="text">
-                This CNFT is designed to donate to African children.
-                <br />
-                Own both CNFT and love.
-              </div>
-            </div>
-            <div className="mainInnerBottom">
-              <div className="title">Comming Soon</div>
-              {/* <div className="counter">
+    <div className="wrap">
+      <div className="main">
+        <div className="mainWrap">
+          <div className="mainTitle">
+            <div className="mainLogo"></div>
+            <br />
+            <span>Cardano</span>Village
+            <div className="mainCount">
+              <div className="countTitle titleWhite">COMING SOON</div>
+              <div className="counter">
                 <div>
                   <span className="days" id="day">
                     {d}
@@ -128,67 +146,98 @@ function Main() {
                   <div className="smallText">Seconds</div>
                 </div>
               </div>
-              <div className="text">September 11th 12PM (UTC)</div> */}
+              <div className="counterDate">December 10th 01PM (UTC)</div>
             </div>
           </div>
         </div>
-        <div className="contents content1">
-          <div className="wrap">
-            <div className="col1">
-              <div className="title">
-                About
-                <br />
-                the project
-              </div>
-            </div>
-            <div className="col2">
-              <div className="text">
-                This project started with Cardanians form South Korea
-                <br />
-                This project, which started from Korea's representative POOL, is CNFT with the goal of donation.
-                <br />
-                Each CNFT automatically donates 80% of the purchase price to Save the Children's ADA wallet, excluding
-                project operating costs.
-                <br />
-                <br />
-                Representative pool name of Korea (in alphabetical order)
-                <br />
-                · DINO & GINO
-                <br />
-                · TERA1 & TERA2
-                <br />· KTP
-              </div>
-            </div>
-          </div>
+
+        <div className="mainBg">
+          <div className="mainImgLeft" />
+          <div className="mainImgRight" />
         </div>
-        <div className="contents content2">
-          <div className="wrap">
-            <div className="col1">
-              <div className="title titleWhite">
-                Donation
-                <br />
-                status and
-                <br />
-                monitoringt
-              </div>
-            </div>
-            <div className="col2">
-              <div className="text">
-                <div className="textBox">
-                  Our address will be revealed later.
-                  <br />
-                  Donations will be made to the public donation address...
-                  <br />
-                  Check it out in Cardano Explorer.
-                  <br />
-                  <a href="https://explorer.cardano.org/" target="_blank" rel="noreferrer">
-                    https://explorer.cardano.org/
-                  </a>
+      </div>
+      <div className="contentsWrap">
+        <div className="contentsTitle">
+          <span> Welcome to </span>
+          <br />
+          Cardano Village
+        </div>
+        <div className="contnetsText">
+          We make Cardano Village. <br />
+          Be a member of this village. <br />
+          Oh, there's our Charles. <br />
+          Let's create a peaceful Cardano CNFT village.
+          <br />
+          <br />
+          <span>Policy ID</span>
+          <br />
+          <div>2c763e2db8337324df122fffa38e0d97ecdf53d139025a0e1b6d6b97</div>
+          <br />
+        </div>
+        <div className="blank" />
+      </div>
+      <div className="wrapGreen">
+        <div className="greenLine" />
+        <div className="contentsWrap">
+          <div className="contentsTitle">
+            <span>Introducing</span>
+            <br />
+            Cardano Village
+          </div>
+          <div className="contnetsText">
+            There is no same house in Cardano Village.
+            <br />
+            Own a house only for you here.
+            <br />
+            Make up a village and a continent.
+            <br />
+            This world is a metaverse in Cardano.
+            <br />
+          </div>
+          <div className="wrapCard">
+            <div className="cardArea">
+              <button>
+                <div className="collectionImg">
+                  <div className="example" />
+                  <img src="/img/village1.png" alt="collection" />
                 </div>
-              </div>
+              </button>
+            </div>
+            <div className="cardArea">
+              <button>
+                <div className="collectionImg">
+                  <div className="example" />
+                  <img src="/img/village2.png" alt="collection" />
+                </div>
+              </button>
+            </div>
+            <div className="cardArea">
+              <button>
+                <div className="collectionImg">
+                  <div className="example" />
+                  <img src="/img/village3.png" alt="collection" />
+                </div>
+              </button>
+            </div>
+            <div className="cardArea">
+              <button>
+                <div className="collectionImg">
+                  <div className="example" />
+                  <img src="/img/village4.png" alt="collection" />
+                </div>
+              </button>
+            </div>
+            <div className="cardArea">
+              <button>
+                <div className="collectionImg">
+                  <div className="example" />
+                  <img src="/img/village5.png" alt="collection" />
+                </div>
+              </button>
             </div>
           </div>
         </div>
+        <div className="blank" />
       </div>
     </div>
   );
