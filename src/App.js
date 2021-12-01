31d4a5274d7dc0f9b3ea6f8d95ced8f37a55c3d1
anna_son <anna.son@jeonbotdae.com>
2021-12-01 13:54:25 +0900

    페이지추가

--- src/App.js
+++ src/App.js
@@ -5,11 +5,27 @@ import { RecoilRoot } from "recoil";
 import ReactGA from "react-ga";
 
 import Header from "./screens/frame/Header";
-import Main from "./screens/Main";
+// 기본 메인
 import Index from "./screens/Index";
-import Guide from "./screens/Guide";
-import ContinentalMap from "./screens/ContinentalMap";
+// 카운트다운 들어간 메인
+import Main from "./screens/Main";
+// 오픈 1차 메인
+import MainOpen1st from "./screens/MainOpen1st";
+// 오픈 1차 마감 메인
+import MainOpen1stClosing from "./screens/MainOpen1stClosing";
+// 오픈 2차 메인
+import MainOpen2st from "./screens/MainOpen2st";
+// 오픈 2차 마감 메인
+import MainOpen2stClosing from "./screens/MainOpen2stClosing";
+// 오픈 3차 메인
+import MainOpen3st from "./screens/MainOpen3st";
+
+import AboutProject from "./screens/AboutProject";
 import Roadmap from "./screens/Roadmap";
+import TeamMember from "./screens/TeamMember";
+import Collection from "./screens/Collection";
+import ContinentalMap from "./screens/ContinentalMap";
+import Faq from "./screens/Faq";
 import ComingSoon from "./screens/ComingSoon";
 import Footer from "./screens/frame/Footer";
 
@@ -23,11 +39,14 @@ function App() {
       <RecoilRoot>
         <Header />
         {isIE || isEdge ? alert("IE / Edge is not supported. Download Chrome/Opera/Firefox") : null}
-        <Route exact path="/" component={Index} />
-        <Route path="/Guide" component={Guide} />
+        <Route exact path="/" component={Main} />
+        <Route path="/Collection" component={Collection} />
+        <Route path="/AboutProject" component={AboutProject} />
         <Route path="/Roadmap" component={Roadmap} />
         <Route path="/ComingSoon" component={ComingSoon} />
+        <Route path="/TeamMember" component={TeamMember} />
         <Route path="/ContinentalMap" component={ContinentalMap} />
+        <Route path="/Faq" component={Faq} />
         <Footer />
       </RecoilRoot>
     </BrowserRouter>
